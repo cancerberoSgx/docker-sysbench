a0ed187061e8baf993a652ed83da0802027aaf97
cancerberosgx <sebastigurin@gmail.com>
2016-09-11 23:39:22 -0300

    initialcontrib

--- hostShared/run-test.sh
+++ hostShared/run-test.sh
@@ -1,6 +1,14 @@
 # docker command : 
 # docker run -itsysbench-alphine sh /shared1/run-test.sh
 
+echo ""
+echo ""
+echo " * * * * CPU TEST * * * "
+
 /shared1/sysbench/sysbench/sysbench --test=cpu run
 
-	
+echo ""
+echo ""
+echo " * * * * MEMORY TEST * * * "
+
+/shared1/sysbench/sysbench/sysbench --test=memory run
